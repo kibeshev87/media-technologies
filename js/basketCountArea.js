bc47e79efc75f0882ec7153cb370cd9b5e92f932
kibeshev87 <mrjimb5@gmail.com>
2023-05-24 17:06:37 +0300

    firstCommit

--- js/basketCountArea.js
+++ js/basketCountArea.js
@@ -1,4 +1,4 @@
-let basketCountAria = document.querySelector('.basket__count')
+let basketCountArea = document.querySelector('.basket__count')
 let basketCount = 0;
 let basketTotalPriceArea = document.querySelector('.basket__price')
 let totalPrice = 0;
@@ -7,7 +7,7 @@ let btn = document.querySelector('.card__btn_inBasket')
 document.onclick = (event) => {
     if (event.target.dataset.price) {
         basketCount++
-        basketCountAria.innerHTML = basketCount;
+        basketCountArea.innerHTML = basketCount;
         totalPrice += Number(event.target.dataset.price);
         basketTotalPriceArea.innerHTML = totalPrice.toLocaleString('ru-Ru') + ' ₽';
         btn.setAttribute('disabled', 'disabled')
